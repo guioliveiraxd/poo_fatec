40d12b2585ed057f2aac6161d02bd8a31ef8a0ba
guioliveiraxd <guioliveira1177@gmail.com>
2026-08-25 11:00:32 -0300

    aula 4

---
+++ aula2508/ProjetoAcampamento/src/Acampamento.java
@@ -0,0 +1,35 @@
+public class AcampamentoTeste {
+    private String nome;
+    private char equipe;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(char equipe) {
+        this.equipe = equipe;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public void imprimir(){
+
+    }
+    public void separarGrupo() {
+
+    }
+}
