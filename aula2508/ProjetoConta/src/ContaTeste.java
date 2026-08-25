c970638897cac095ba9f30915d700841f357b227
guioliveiraxd <guioliveira1177@gmail.com>
2026-08-25 12:16:25 -0300

    aula 4

---
+++ aula2508/ProjetoConta/src/ContaTeste.java
@@ -0,0 +1,23 @@
+public class ContaTeste {
+    public Conta cc = new Conta();
+
+    public static void main(String[] args) {
+
+    }
+
+    public void execCadastrar() {
+
+    }
+
+    public void execConsultar() {
+
+    }
+
+    public void execSacar() {
+
+    }
+    public void execDepositar(){
+
+    }
+
+}
